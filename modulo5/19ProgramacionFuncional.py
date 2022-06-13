eff3fb22a9f44142ede70290e02b42eb93179389
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2022-06-13 11:22:50 -0500

    Ejemplo basico de una funcion Lambda

---
+++ modulo5/19ProgramacionFuncional.py
@@ -0,0 +1,13 @@
+# Funciones LAMBDA o ANONIMAS
+# Funcion normal
+def duplicar(num):
+  return num*2
+
+duplicado= duplicar(2)
+print(duplicado)
+
+
+
+# Funcion Lambda
+duplicado=lambda num: num*2
+print(duplicado(3))
