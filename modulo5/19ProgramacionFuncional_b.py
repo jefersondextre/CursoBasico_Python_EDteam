38c4247e4c8baf6129f9e24048430317a6bcbc10
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2022-06-13 17:49:52 -0500

    VIDEO 5.1PROGRAMACION FUNCIONAL: Lambda

---
+++ modulo5/19ProgramacionFuncional_b.py
@@ -0,0 +1,11 @@
+def mostrar_mensaje(mensaje):
+  return f'Su mensaje es {mensaje}'
+
+mensaje_mostrado = mostrar_mensaje('Hola')
+print(mensaje_mostrado)
+
+
+
+# Funcion Lambda
+mensaje_mostrado =lambda mensaje: f'Su mensaje es {mensaje}'
+print(mensaje_mostrado('Bienvenido'))
